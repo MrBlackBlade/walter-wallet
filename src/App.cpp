4f954bb9dfafbbd52ce68c10b37a680552698043
Boldingar <m.a.7snen@gmail.com>
2024-04-14 21:18:53 +0200

    Waltuh ( Added an icon to the program )

--- src/App.cpp
+++ src/App.cpp
@@ -8,11 +8,14 @@ bool App::OnInit() {
 	AttachConsole(ATTACH_PARENT_PROCESS);
 	freopen("CONOUT$", "w", stdout);
 	freopen("CONIN$", "r", stdin);
-	
+
+	wxIcon icon(wxT("resources\\walterWallet.ico"), wxBITMAP_TYPE_ICO);
+
 	LoginFrame* loginFrame = new LoginFrame("Walter Wallet");
 	loginFrame->SetClientSize(450, 700);
 	loginFrame->Center();
 	loginFrame->Show();
+	loginFrame->SetIcon(icon);
 	return true;
 }
 wxIMPLEMENT_APP(App);
